fddc4ebe37ad8cf35698bb187b6d36bc697eb9eb
nicopoliakov <nicolas.pollak@hei.fr>
2014-11-17 16:18:45 +0100

    Création du service MembreService + implémentation MembreServiceImpl

--- Agile/src/main/java/hei/agile/entity/Membre.java
+++ Agile/src/main/java/hei/agile/entity/Membre.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-public class Membre {
+public class Membre{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)

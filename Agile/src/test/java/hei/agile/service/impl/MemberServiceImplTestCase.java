0741dbf08c69ca6667d664e9a5fa90772311e64b
cml-big <Charles@Big-PC>
2014-12-09 09:31:07 +0100

    test unitaire -> memberDAO NullPointer

--- Agile/src/test/java/hei/agile/service/impl/MemberServiceImplTestCase.java
+++ Agile/src/test/java/hei/agile/service/impl/MemberServiceImplTestCase.java
@@ -2,38 +2,21 @@ package hei.agile.service.impl;
 
 import hei.agile.dao.MemberDAO;
 import hei.agile.entity.Member;
-import hei.agile.service.MemberService;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-@Named
 @Transactional
-public class MemberServiceImpl implements MemberService {
-	
+public class MemberServiceImplTestCase {
+
 	@Inject
 	private MemberDAO memberDAO;
-
-	@Override
-	public void saveMember(Member member) {
-		
-		memberDAO.save(member);
-	}
-
-	@Override
-	public List<Member> findAll() {
-		
-		List<Member> members = memberDAO.findAll();
-		return members;
-	}
 	
 	@Test
 	public void saveMember() {
